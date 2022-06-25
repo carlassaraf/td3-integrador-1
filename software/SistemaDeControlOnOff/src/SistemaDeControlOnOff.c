e66999c5f5e8f89574d25d935c6ad346a9f226c3
GustavoSuarez10 <laimesuarezgustavo12@gmail.com>
2022-06-24 21:54:41 -0300

    tareas de SD

--- software/SistemaDeControlOnOff/src/SistemaDeControlOnOff.c
+++ software/SistemaDeControlOnOff/src/SistemaDeControlOnOff.c
@@ -7,45 +7,146 @@
  Description : main definition
 ===============================================================================
 */
-
-#if defined (__USE_LPCOPEN)
-#if defined(NO_BOARD_LIB)
-#include "chip.h"
-#else
+#include <stdlib.h>
 #include "board.h"
-#endif
-#endif
+#include "chip.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
 
-#include <cr_section_macros.h>
+#include "SPI.h"
+#include "sd.h"
 
 // TODO: insert other include files here
 
 // TODO: insert other definitions and declarations here
+typedef struct{
+	CARDCONFIG CardConfig;
+	SD_BOOL tipo; 	// tipo se usa para ver el la version de la SD pero en este caso no es necesario, si para poder utilziar la funcion INIT
+    FRESULT fr; 	// variable para leer los retornos de las funciones FATS
+    FATFS fs; 		// Variable tipo FATS
+    FIL fil; 		// Archivos
+    uint32_t tamanioArchivo;
+    UINT BytesWritten;
+    UINT ByteRead;
+    char bufferWrite[20]; // Buffer
+    char *bufferRead;
+}SD_Variables;
+
+
+/* Handler de la cola */
+xQueueHandle queueADC;
+
+static void SD_CreaCarpeta(void *pvParameters){
+
+    SD_Variables carpeta;
+
+    /* Inicializamos y verificamos que se inicialice correctamente el SD*/
+    if (SD_Init (&carpeta.tipo) == SD_FALSE)
+    {
+    	// No se pudo iniciar la tarjeta. por favor, revisa la tarjeta
+        while (1);
+    }
+
+    /* Verificamos que se pueda leer */
+    if (SD_ReadConfiguration (&carpeta.CardConfig) == SD_FALSE)
+    {
+    	// No se pudo leer
+		while (1);
+    }
+    // Se inicializó con exito.
+
+	/* Abro una carpeta que exista o creo una nueva */
+    carpeta.fr = f_mount(&carpeta.fs, "0:", 0); // Registro un objeto del sistema de archivos para una unidad lógica "0:", es decir es el Driver.
+    carpeta.fr = f_open(&carpeta.fil, "TD3.txt", FA_WRITE | FA_CREATE_ALWAYS); // Si no existe crea.
+    if (carpeta.fr == FR_OK) {
+    	carpeta.fr = f_write (&carpeta.fil, carpeta.bufferWrite, sizeof(carpeta.bufferWrite), &carpeta.BytesWritten); // Escribe
+    	// Buscar hasta el final del archivo para agregar datos
+    	carpeta.fr = f_lseek(&carpeta.fil, f_size(&carpeta.fil));
+
+        // En caso de que no poder escribirse la carpeta se debe cerrarla.
+        if (carpeta.fr != FR_OK)
+            f_close(&carpeta.fil);
+    }
+
+    // Verifico que se haya creado correctamente.
+	if (carpeta.fr != FR_OK)
+	{
+		// Si no crea nada, viene acá.
+		while (1);
+	}
+	// Se creó la carpeta TecnicasDigitalesIII, debemos cerrar la carpeta.
+	f_close(&carpeta.fil);
+
+	while(1)
+	{
+
+	}
+}
+
+static void SD_LeeCarpeta(void *pvParameters){
+
+    SD_Variables carpeta;
+
+	while(1){
+
+    	/* Abro una carpeta e imprimo los datos */
+		carpeta.fr = f_mount(&carpeta.fs, "0:", 0); // Driver para la carpeta
+
+    	// Busco la carpeta, en caso de no existir fr sera distinto de FR_OK
+    	carpeta.fr = f_open(&carpeta.fil, "TD3.txt", FA_READ);
+    	if (carpeta.fr == FR_OK)
+    	{
+    		carpeta.tamanioArchivo = f_size(&carpeta.fil);
+    		carpeta.bufferRead = malloc(carpeta.tamanioArchivo);
+    		while(carpeta.tamanioArchivo > 0)
+    		{
+    			// Leemos el contenido de la carpeta, en FR tiene que devolver FR_OK si se leyó correctamente
+    			carpeta.fr = f_read (&carpeta.fil, carpeta.bufferRead, (UINT)sizeof(carpeta.bufferWrite), &carpeta.ByteRead);
+    			carpeta.tamanioArchivo -= sizeof(carpeta.bufferWrite); //
+    		}
+    		// Buscar hasta el final del archivo para agregar datos
+    		carpeta.fr = f_lseek(&carpeta.fil, f_size(&carpeta.fil));
+
+    		// Si no se pudo leer nada cierro la carpeta
+    		if (carpeta.fr != FR_OK)
+    			f_close(&carpeta.fil);
+    	}
+
+    	// Una vez leido se cierra la carpeta
+    	f_close(&carpeta.fil);
+	}
+
+}
 
 int main(void) {
 
-#if defined (__USE_LPCOPEN)
     // Read clock settings and update SystemCoreClock variable
     SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
-#endif
-#endif
+	/* Sets up DEBUG UART */
+	DEBUGINIT();
 
-    // TODO: insert code here
+    /* Inicializo SPI */
+    SPI_Inicializar();
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
+	/* Creacion de la cola */
+	queueADC = xQueueCreate(1,sizeof(uint16_t));
+
+    /* Creacion de tareas */
+	xTaskCreate(SD_CreaCarpeta, (char *) "Temporizacion",
+    			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+    			(xTaskHandle *) NULL);
+    xTaskCreate(SD_LeeCarpeta, (char *) "Lectura",
+        		configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 2UL),
+        		(xTaskHandle *) NULL);
+
+    /* Arranca el scheduler */
+	vTaskStartScheduler();
+
+	/* Nunca debería arribar aquí */
+
     while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
+
     }
     return 0 ;
 }
---
+++ software/SistemaDeControlOnOff/src/diskio.c
@@ -0,0 +1,191 @@
+/*-----------------------------------------------------------------------*/
+/* Low level disk I/O module SKELETON for FatFs     (C)ChaN, 2019        */
+/*-----------------------------------------------------------------------*/
+/* If a working storage control module is available, it should be        */
+/* attached to the FatFs via a glue function rather than modifying it.   */
+/* This is an example of glue functions to attach various exsisting      */
+/* storage control modules to the FatFs module with a defined API.       */
+/*-----------------------------------------------------------------------*/
+
+#include "ff.h"			/* Obtains integer types */
+#include "diskio.h"		/* Declarations of disk functions */
+#include "sd.h"
+#include "SPI.h"
+#include "stdio.h"
+#include "board.h"
+
+/* Definitions of physical drive number for each drive */
+#define DEV_RAM		0	/* Example: Map Ramdisk to physical drive 0 */
+#define DEV_MMC		1	/* Example: Map MMC/SD card to physical drive 1 */
+#define DEV_USB		2	/* Example: Map USB MSD to physical drive 2 */
+
+/** Select the card */
+#define SD_Select()  do {SPI_Pin(SSEL0, Bajo);} while (0)
+/** DeSelect the card */
+#define SD_DeSelect() do {SPI_Pin(SSEL0, Alto); SD_RecibirByte();} while (0)
+
+DSTATUS stat;
+/*-----------------------------------------------------------------------*/
+/* Get Drive Status                                                      */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_status (
+	BYTE drv		/* Physical drive number (0) */
+)
+{
+	if (drv) return STA_NOINIT;		/* Supports only single drive */
+
+	return stat;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_initialize (
+	BYTE drv				/* Physical drive nmuber to identify the drive */
+)
+{
+	//DSTATUS stat;
+	uint8_t tipo;
+	CARDCONFIG *CConfig;
+	if (drv) return STA_NOINIT;			/* Supports only single drive */
+//	if (stat & STA_NODISK) return stat;	/* No card in the socket */
+
+	//if (SD_Init(&tipo) && SD_ReadConfiguration(CConfig))
+		//stat &= ~STA_NOINIT;
+
+	return stat;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read (
+	BYTE drv,			/* Physical drive number (0) */
+	BYTE *buff,			/* Pointer to the data buffer to store read data */
+	DWORD sector,		/* Start sector number (LBA) */
+	BYTE count			/* Sector count (1..255) */
+)
+{
+	if (drv || !count) return RES_PARERR;
+	if (stat & STA_NOINIT) return RES_NOTRDY;
+
+	if (SD_ReadSector (sector, buff, count) == SD_TRUE)
+		return RES_OK;
+	else
+		return RES_ERROR;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+/*-----------------------------------------------------------------------*/
+
+#if _READONLY == 0
+DRESULT disk_write (
+	BYTE drv,			/* Physical drive number (0) */
+	const BYTE *buff,	/* Pointer to the data to be written */
+	DWORD sector,		/* Start sector number (LBA) */
+	BYTE count			/* Sector count (1..255) */
+)
+{
+	if (drv || !count) return RES_PARERR;
+	if (stat & STA_NOINIT) return RES_NOTRDY;
+//	if (stat & STA_PROTECT) return RES_WRPRT;
+
+	if ( SD_WriteSector(sector, buff, count) == SD_TRUE)
+		return RES_OK;
+	else
+		return 	RES_ERROR;
+
+}
+#endif /* _READONLY == 0 */
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_ioctl (
+	BYTE drv,		/* Physical drive number (0) */
+	BYTE ctrl,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+	DRESULT res;
+	BYTE n, *ptr = buff;
+	uint8_t CardType;          /* card type */
+	CARDCONFIG CardConfig;     /* Card configuration */
+
+	if (drv) return RES_PARERR;
+	if (stat & STA_NOINIT) return RES_NOTRDY;
+
+	res = RES_ERROR;
+
+	switch (ctrl) {
+	case CTRL_SYNC :		/* Make sure that no pending write process */
+        SD_Select();
+		if (SD_WaitForReady() == SD_TRUE)
+			res = RES_OK;
+		break;
+
+	case GET_SECTOR_COUNT :	/* Get number of sectors on the disk (DWORD) */
+		*(DWORD*)buff = CardConfig.sectorcnt;
+		res = RES_OK;
+		break;
+
+	case GET_SECTOR_SIZE :	/* Get R/W sector size (WORD) */
+		*(WORD*)buff = CardConfig.sectorsize;	//512;
+		res = RES_OK;
+		break;
+
+	case GET_BLOCK_SIZE :	/* Get erase block size in unit of sector (DWORD) */
+		*(DWORD*)buff = CardConfig.blocksize;
+		res = RES_OK;
+		break;
+
+	case MMC_GET_TYPE :		/* Get card type flags (1 byte) */
+		*ptr = CardType;
+		res = RES_OK;
+		break;
+
+	case MMC_GET_CSD :		/* Receive CSD as a data block (16 bytes) */
+		for (n=0;n<16;n++)
+			*(ptr+n) = CardConfig.csd[n];
+		res = RES_OK;
+		break;
+
+	case MMC_GET_CID :		/* Receive CID as a data block (16 bytes) */
+		for (n=0;n<16;n++)
+			*(ptr+n) = CardConfig.cid[n];
+		res = RES_OK;
+		break;
+
+	case MMC_GET_OCR :		/* Receive OCR as an R3 resp (4 bytes) */
+		for (n=0;n<4;n++)
+			*(ptr+n) = CardConfig.ocr[n];
+		res = RES_OK;
+		break;
+
+	case MMC_GET_SDSTAT :	/* Receive SD status as a data block (64 bytes) */
+		for (n=0;n<64;n++)
+            *(ptr+n) = CardConfig.status[n];
+        res = RES_OK;
+		break;
+
+	default:
+		res = RES_PARERR;
+	}
+
+    SD_DeSelect();
+
+	return res;
+}
+
