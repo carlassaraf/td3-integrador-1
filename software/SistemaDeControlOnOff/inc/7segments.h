8181b50cc343a7fc7a5376eba396fdc5ee2e81d8
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-06-27 10:34:09 -0300

    docs y macros nuevas

--- software/SistemaDeControlOnOff/inc/7segments.h
+++ software/SistemaDeControlOnOff/inc/7segments.h
@@ -10,28 +10,42 @@
 
 #include "chip.h"
 
-#define SEG_A	(1 << 0)
-#define SEG_B	(1 << 1)
-#define SEG_C	(1 << 2)
-#define SEG_D	(1 << 3)
-#define SEG_E	(1 << 4)
-#define SEG_F	(1 << 5)
-#define SEG_G	(1 << 6)
-#define SEG_DP	(1 << 7)
+/* Port de los segmentos y comunes */
+#define SEG_PORT	LPC_GPIO2, 2
+#define	DIGIT_PORT	LPC_GPIO, 0
 
-#define DP	7
-
+/* Pines de los segmentos */
+#define SEG_A	0
+#define	SEG_B	1
+#define SEG_C	2
+#define SEG_D	3
+#define	SEG_E	4
+#define SEG_F	5
+#define SEG_G	6
+#define SEG_DP		7
+/* Mascaras para los pines de los segmentos */
+#define SEG_A_MASK	(1 << SEG_A)
+#define SEG_B_MASK	(1 << SEG_B)
+#define SEG_C_MASK	(1 << SEG_C)
+#define SEG_D_MASK	(1 << SEG_D)
+#define SEG_E_MASK	(1 << SEG_E)
+#define SEG_F_MASK	(1 << SEG_F)
+#define SEG_G_MASK	(1 << SEG_G)
+#define SEG_DP_MASK	(1 << SEG_DP)
+/* Pines de que controlan el comun de los segmentos */
 #define DIGIT_1	10
 #define DIGIT_2	5
 #define DIGIT_3	4
+/* Mascaras para los pines del comun */
+#define DIGIT_1_MASK	(1 << DIGIT_1)
+#define DIGIT_2_MASK	(1 << DIGIT_2)
+#define DIGIT_3_MASK	(1 << DIGIT_3)
+/* Mascara completa de los pines que controlan el comun */
+#define DIGITS 		DIGIT_1_MASK | DIGIT_2_MASK | DIGIT_3_MASK
+/* Mascara completa de los pines que van a los segmentos */
+#define SEGMENTS	SEG_A_MASK | SEG_B_MASK | SEG_C_MASK | SEG_D_MASK | SEG_E_MASK | SEG_F_MASK | SEG_G_MASK | SEG_DP_MASK
 
-#define DIG_1	(1 << 10)
-#define DIG_2	(1 << 5)
-#define DIG_3	(1 << 4)
-
-#define DIGITS 		DIG_1 | DIG_2 | DIG_3
-#define SEGMENTS	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G | SEG_DP
-
+/* Prototipos de funciones */
 void gpio_7segments_init(void);
 void gpio_7segments_clear(void);
 void gpio_7segments_write(uint8_t number);
--- software/SistemaDeControlOnOff/src/7segments.c
+++ software/SistemaDeControlOnOff/src/7segments.c
@@ -7,7 +7,7 @@
 
 #include "7segments.h"
 
-/* Para catodo ocmun */
+/* Para anodo ocmun */
 uint8_t numbers[] = {
 	~0x3f,  	/* 0 */
 	~0x06,  	/* 1 */
@@ -19,21 +19,32 @@ uint8_t numbers[] = {
 	~0x07,  	/* 7 */
 	~0x7f,  	/* 8 */
 	~0x67,  	/* 9 */
-	~0x00	/* Clear */
+	~0x00		/* Clear */
 };
 
+/*
+ * 	@brief	Inicializa los pines conectados al 7 segmentos
+ */
 void gpio_7segments_init(void) {
 	/* Inicializo los pines como salidas */
-	Chip_GPIO_SetPortDIROutput(LPC_GPIO, 0, DIGITS);
-	Chip_GPIO_SetPortDIROutput(LPC_GPIO2, 2, SEGMENTS);
+	Chip_GPIO_SetPortDIROutput(DIGIT_PORT, DIGITS);
+	Chip_GPIO_SetPortDIROutput(SEG_PORT, SEGMENTS);
 	Chip_IOCON_Init(LPC_IOCON);
 }
 
+/*
+ * 	@brief	Limpia el 7 segmentos
+ */
 void gpio_7segments_clear(void) {
 	/* Apago todos los segmentos */
 	gpio_7segments_write(10);
 }
 
+/*
+ * 	@brief	Escribe el numero en el segmento
+ *
+ * 	@param	number: numero del 0 al 9
+ */
 void gpio_7segments_write(uint8_t number) {
 	/* Obtengo el valor que hay que escribir */
 	uint8_t val = numbers[number];
@@ -42,20 +53,27 @@ void gpio_7segments_write(uint8_t number) {
 		/* Si el bit de esa posicion esta en uno, true, sino, false */
 		bool state = (val & (1 << pin))? true : false;
 		/* Escribo el valor del pin */
-		Chip_GPIO_SetPinState(LPC_GPIO2, 2, pin, state);
+		Chip_GPIO_SetPinState(SEG_PORT, pin, state);
 	}
 }
 
+/*
+ * 	@brief	Prende el digito elegido
+ *
+ * 	@param	number: DIGITO_1, DIGITO_2 o DIGITO_3
+ */
 void gpio_7segments_set_digit(uint8_t number) {
 	/* Apago los tres 7 segmentos */
-	Chip_GPIO_SetPinState(LPC_GPIO, 0, DIGIT_1, true);
-	Chip_GPIO_SetPinState(LPC_GPIO, 0, DIGIT_2, true);
-	Chip_GPIO_SetPinState(LPC_GPIO, 0, DIGIT_3, true);
+	Chip_GPIO_SetPinState(DIGIT_PORT, DIGIT_1, true);
+	Chip_GPIO_SetPinState(DIGIT_PORT, DIGIT_2, true);
+	Chip_GPIO_SetPinState(DIGIT_PORT, DIGIT_3, true);
 	/* Prendo el digito solicitado */
-	Chip_GPIO_SetPinState(LPC_GPIO, 0, number, false);
+	Chip_GPIO_SetPinState(DIGIT_PORT, number, false);
 }
 
-void gpio_7segments_set_dp(bool on) {
-
-	Chip_GPIO_SetPinState(LPC_GPIO2, 2, DP, on);
-}
+/*
+ * 	@brief	Prende/Apaga el punto del 7 segmentos
+ *
+ * 	@param	on: true para prenderlo, false para apagarlo
+ */
+void gpio_7segments_set_dp(bool on) { Chip_GPIO_SetPinState(SEG_PORT, SEG_DP, on); }
